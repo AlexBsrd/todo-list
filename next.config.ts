26779277cb6cacbb9815d908f8a511cbcc6b0a7d
Alex Broussard <alex.broussard@itesoft.com>
2025-02-03 21:50:18 +0100

    fix(deploy): let deploy.yml handle next.js configuration (basePath and image optimization) in order to not affect local development

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export', // nécessaire de générer une version statique pour github Pages
-    basePath: '/todo-list', // Remplacez par le nom de votre dépôt GitHub
-    images: {
-        unoptimized: true, // nécessaire pour l'export statique
-    },
 };
 
 export default nextConfig;
