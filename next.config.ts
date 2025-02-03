e2c0a477ebd0c5aeb8e8cd7ff1a47cf47c4d260f
Alex Broussard <alex.broussard@itesoft.com>
2025-02-03 21:04:50 +0100

    feat(deploy): Auto-deploy to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export', // nécessaire de générer une version statique pour github Pages
+    basePath: '/todo-list', // Remplacez par le nom de votre dépôt GitHub
+    images: {
+        unoptimized: true, // nécessaire pour l'export statique
+    },
 };
 
 export default nextConfig;
